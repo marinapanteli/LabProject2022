3c46fe6288e666952d1c27baff9fee016413b383
markrobinsonuzh <markrobinsonuzh@users.noreply.github.com>
2022-03-18 21:59:14 +0100

    fix buggy get_read_ranges that I introduced before

--- Project/functions_1.R
+++ Project/functions_1.R
@@ -1,45 +1,19 @@
-# get_read_ranges <- function(ga) {
-#   nm_new <- names(ga)
-#   rngs_new <- cigarRangesAlongReferenceSpace(cigar(ga),
-#                                          pos = start(ga),
-#                                          drop.empty.ranges = FALSE,
-#                                          N.regions.removed = FALSE,
-#                                          with.ops = TRUE,
-#                                          reduce.ranges = FALSE) %>%
-#     setNames(nm_new)
-# 
-#   nms_new <- rep(nm_new, lengths(rngs_new))
-#   rngs_u_new <- unlist(rngs_new, use.names = FALSE)
-# 
-#   keep_new <- names(rngs_u_new) != "N"
-#   rngs_new <- split(rngs_u_new[keep_new], nms_new[keep_new])[nm_new]
-# 
-#   dokimh2  <- lapply(rngs_new, function(u) {
-#     nm_new <- names(u)
-#     names(u) <- NULL
-#     u[]
-#   })
-# 
-#   dokimh2
-# 
-# }
 get_read_ranges <- function(ga) {
-  rngs <- cigarRangesAlongReferenceSpace(cigar(ga),
+  nm <- names(ga)
+  rngs <- cigarRangesAlongReferenceSpace(cigar(ga), 
                                          pos = start(ga),
                                          drop.empty.ranges = FALSE,
                                          N.regions.removed = FALSE,
                                          with.ops = TRUE,
-                                         reduce.ranges = FALSE)
-  names(rngs) <- names(ga)
-  # remove 'N' ranges
-  rngs <- lapply(rngs, function(u) {
-    nm <- names(u)
-    names(u) <- NULL
-    u[nm != "N"]
-  })
-  IRangesList(rngs)
+                                         reduce.ranges = FALSE) %>% 
+    setNames(nm)
+  
+  nms <- rep(nm, lengths(rngs))
+  rngs_u <- unlist(rngs, use.names = FALSE)
+  
+  keep <- names(rngs_u) != "N"
+  split(unname(rngs_u[keep]), nms[keep])[nm]
 }
-
 
 extract_exons <- function(x, gene = NULL) {
   x_ex <- x[x$type=="exon" & seqnames(x) != "chrM"]
